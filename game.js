c20b9b36769a1bca93aa23dd50b530fab1ba08d5
robbie6153-cmd <robbie6153@icloud.com>
2026-02-26 14:56:21 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+// Confirm script is loaded
+console.log('Game script loaded');
+
+// Hook up Start Game button
 document.getElementById('start').addEventListener('click', function() {
-  alert('Button works!');
+  alert('Button works!'); // temporary test
+  document.getElementById('game-container').innerHTML = "<p>Game started!</p>";
 });
